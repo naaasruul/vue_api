dbdeec8e7932ab7e38c5a1dd1291105a03e722f2
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-01-19 22:38:00 +0800

    use axios to get data

--- src/components/users.vue
+++ src/components/users.vue
@@ -1,25 +1,42 @@
 <template>
     <div class="row">
-        <div class="col-auto mb-4">
+        <div class="col-auto mb-4" v-for="user in data.users" :key="user.id">
             <div class="card" style="width: 14rem;">
-                <img 
-                    class="card-img-top" 
-                    src="https://placebeard.it/300/300"
-                    alt="Card image cap"
-                >
+                <img class="card-img-top" :src="'https://placebeard.it/300/300?id=' + user.id" alt="Card image cap" />
                 <div class="card-body">
                     <div class="card-text">
-                        <strong>Name:</strong> Francis
+                        <strong>Name:</strong> {{ user.name }}
                     </div>
                     <div class="card-text">
-                        <strong>Lastname:</strong>  Jones
+                        <strong>Lastname:</strong> {{ user.lastname }}
                     </div>
                 </div>
             </div>
-         </div>
+        </div>
     </div>
 </template>
 
 <script setup>
+import axios from "axios";
+import { onMounted, reactive } from "vue";
 
+const data = reactive({
+    users: [],
+});
+
+const loadUsers = () => {
+    axios
+        .get("http://localhost:3004/users")
+        .then((response) => {
+            data.users = response.data;
+            console.log(response.data);
+        })
+        .catch((error) => {
+            console.error("Error fetching users:", error);
+        });
+};
+
+onMounted(() => {
+    loadUsers();
+});
 </script>
